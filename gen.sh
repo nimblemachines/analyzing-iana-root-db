d6d07510f22c8ceddd00f3d8daa5e8796e3a176e
David Frech <david@nimblemachines.com>
2018-08-14 19:04:08 -0700

    let user specify a date to gen.sh

--- gen.sh
+++ gen.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
 # We are date-stamping everything now.
-stamp=$(date "+%F")
+
+# If $1 defined, use it as the stamp; otherwise, use today's date.
+if [ $1 ]; then
+   stamp=$1
+else
+   stamp=$(date "+%F")
+fi
+
+echo Generating for $stamp
 
 # Run the script to create a Docs sheet of the current db.
 lua extract-tlds.lua ${stamp}_root-db.html sheet > ${stamp}_root-db.txt
